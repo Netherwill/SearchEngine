9f6b18dd52c9fc148d99432b73a54e5d02fd2b33
taozeyi <taozeyi@gmail.com>
2014-10-21 19:19:30 -0500

    field

--- src/edu/uiowa/cs2820/engine/search/Field.java
+++ src/edu/uiowa/cs2820/engine/search/Field.java
@@ -2,5 +2,19 @@ package edu.uiowa.cs2820.engine.search;
 
 public class Field {
 	private String fieldName;
+	private	String fieldValue;
 	
+	public Field(String fieldname, String fieldValue){
+		this.fieldName=fieldname;
+		this.fieldValue=fieldValue;		
+	}
+	public String getFieldName(){
+		return fieldName;
+	}
+	public String getFieldValue(){
+		return fieldValue;
+	}
+	public String toString(){
+		return fieldName + ":" + fieldValue;
+	}
 }
--- src/edu/uiowa/cs2820/engine/test/fieldTest.java
+++ src/edu/uiowa/cs2820/engine/test/fieldTest.java
@@ -1,5 +1,17 @@
 package edu.uiowa.cs2820.engine.test;
 
+import edu.uiowa.cs2820.engine.search.Field;
+import static org.junit.Assert.*;
+import org.junit.*;
+
 public class fieldTest {
+	@Test
+	public void fieldConstructorTest(){
+		String s1="ISBN";
+		String v1="123456789";
+		Field testfield = new Field(s1,v1);
+		assertEquals(testfield.getFieldName(),s1);
+		assertEquals(testfield.getFieldValue(),v1);
+	}
 
 }

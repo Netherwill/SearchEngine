0c5214eec01edca9a108d1d87e898306a79bdf38
taozeyi <taozeyi@gmail.com>
2014-10-21 19:44:19 -0500

    integration test

--- src/edu/uiowa/cs2820/engine/test/integrationTest.java
+++ src/edu/uiowa/cs2820/engine/test/integrationTest.java
@@ -1,5 +1,17 @@
 package edu.uiowa.cs2820.engine.test;
 
+import edu.uiowa.cs2820.engine.search.Field;
+import edu.uiowa.cs2820.engine.search.Database;
+import edu.uiowa.cs2820.engine.search.Indexer;
+import edu.uiowa.cs2820.engine.search.FieldSearch;
+
+import org.junit.*;
+
 public class integrationTest {
+
+	@Test
+	public void databaseIntegrationTest(){
+		
+	}
 
 }

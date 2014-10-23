2cac033baadd1d97497d1870d4d7c9101ecb321e
Khashi Reyes <khashi-reyes@uiowa.edu>
2014-10-23 15:21:36 -0500

    Update Database.java

--- src/edu/uiowa/cs2820/engine/search/Database.java
+++ src/edu/uiowa/cs2820/engine/search/Database.java
@@ -4,46 +4,71 @@
 
 package org.uiowa.cs2820.engine;
 
-import java.util.Vector;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Set;
 
-public class Database 
-{	
-	public static Vector<Data> v = new Vector<Data>();
+public class Database {
 	
-	public static void store(String field, String id) {
-		Data d = new Data(field,id);
-		v.add(d);
+	private HashMap<Field, ArrayList<String>> map;
+	
+	public Database(){
+		map=new HashMap<Field,ArrayList<String>>();
 	}
 	
-	public static String getField(int i) 
-	{
-		return v.get(i).Field();
-	}
-	
-	public static String getId(int i) 
-	{
-		return v.get(i).Id();
-	}
-	
-	public static int getSize() {
-		return v.size();
-	}
-
-	static class Data 
-	{
-		String field;
-		String id;
-		Data(String field, String id) {
-			this.field = field;
-			this.id = id;
+	public void store(Field f, String identifier) {
+		if(map.containsKey(f)){
+			map.get(f).add(identifier);
 		}
-		public String Field() 
-		{
-			return field;
-		}
-		public String Id() 
-		{
-			return id;
+		else{
+			ArrayList<String> newIdentifier =new ArrayList<String>();
+			newIdentifier.add(identifier);
+			map.put(f, newIdentifier);
 		}
 	}
+	public ArrayList<String> getValue(Field f) throws Exception{
+		ArrayList<String> result=null;
+		if(map.containsKey(f)){
+			result=map.get(f);
+		}else{
+			throw new Exception("no such field in Database");
+		}
+		return result;
+		
+	}
+	
+	public int getSize(){
+		return map.keySet().size();
+	}
+	
+	public Set<Field> getKey(){
+		Set<Field> keySet=null;
+		keySet=map.keySet();
+		return keySet;
+	}
+	
+	public boolean isEmpty(){
+		if(map.isEmpty()){
+			return false;
+		}
+		return true;
+	}
+	
+	//only for test purpose never use for querying
+	public String querySingleField(Field f) throws Exception{
+		String temp="";
+		if(map.get(f)==null){
+			throw new Exception("there is no such field in database");
+			
+		}else{
+			
+			ArrayList<String> resultList = new ArrayList<String>();
+			resultList=map.get(f);
+			for(int i=0; i<resultList.size();i++){
+				temp+=resultList.get(i);
+				}
+		}
+		return temp;
+	}
+		
 }

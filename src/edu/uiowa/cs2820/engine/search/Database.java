d3d11e60f0179d778854a56af3f0dd67a00cd982
kreyes29 <khashi-reyes@uiowa.edu>
2014-10-23 01:58:00 -0500

    Update Database.java

--- src/edu/uiowa/cs2820/engine/search/Database.java
+++ src/edu/uiowa/cs2820/engine/search/Database.java
@@ -1,5 +1,43 @@
-package edu.uiowa.cs2820.engine.search;
+package org.uiowa.cs2820.engine;
 
-public class Database {
+import java.util.Vector;
+import org.uiowa.cs2820.engine.Field;
 
+public class Database 
+{	
+	private static Vector<Data> v;
+	
+	public static void store(Field field, String id) 
+	{
+		Data d = new Data(field,id);
+		v.add(d);
+	}
+	
+	public static Field retrieveField(int index) 
+	{
+		return v.get(index).getField();
+	}
+	
+	public static String retrieveId(int index) 
+	{
+		return v.get(index).getId();
+	}
+	
+	static class Data 
+	{
+		private Field field;
+		private String id;
+		Data(Field field, String id) {
+			this.field = field;
+			this.id = id;
+		}
+		Field getField() 
+		{
+			return field;
+		}
+		String getId() 
+		{
+			return id;
+		}
+	}
 }

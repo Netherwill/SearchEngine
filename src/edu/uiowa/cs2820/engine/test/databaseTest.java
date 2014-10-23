13924f8643b7d6244c018ff2d5c48fdb4ee530de
kreyes29 <khashi-reyes@uiowa.edu>
2014-10-23 13:18:13 -0500

    Update databaseTest.java

--- src/edu/uiowa/cs2820/engine/test/databaseTest.java
+++ src/edu/uiowa/cs2820/engine/test/databaseTest.java
@@ -1,5 +1,24 @@
-package edu.uiowa.cs2820.engine.test;
+/* Team 7
+   Members: Khashi Reyes, Justin Shroyer, Yicheng Zhao, Tao Zeyi
+*/
 
-public class databaseTest {
+package org.uiowa.cs2820.engine;
+
+import static org.junit.Assert.*;
+
+import org.uiowa.cs2820.engine.Database;
+import org.junit.Test;
+
+public class DatabaseTest {
+
+	@Test
+	public void DataTest() {
+		String s1 = "Hello";
+		String s2 = "World";
+		Database d = new Database();
+		d.store(s1, s2);
+		assertEquals(d.getField(0), s1);
+		assertEquals(d.getId(0), s2);
+	}
 
 }

726d0ae87d073987cf25969920da7a97295ed6fc
kreyes29 <khashi-reyes@uiowa.edu>
2014-10-23 10:38:57 -0500

    Update Database.java

--- src/edu/uiowa/cs2820/engine/search/Database.java
+++ src/edu/uiowa/cs2820/engine/search/Database.java
@@ -7,18 +7,18 @@ public class Database
 {	
 	private static Vector<Data> v;
 	
-	public static void store(String field, String id) 
+	public void store(String field, String id) 
 	{
 		Data d = new Data(field,id);
 		v.add(d);
 	}
 	
-	public static String retrieveField(int index) 
+	public String retrieveField(int index) 
 	{
 		return v.get(index).getField();
 	}
 	
-	public static String retrieveId(int index) 
+	public String retrieveId(int index) 
 	{
 		return v.get(index).getId();
 	}

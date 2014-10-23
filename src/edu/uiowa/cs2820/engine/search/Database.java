bfbea83ae44a6fa979c507b24c2aab67d672c5e5
kreyes29 <khashi-reyes@uiowa.edu>
2014-10-23 13:19:08 -0500

    Update Database.java

--- src/edu/uiowa/cs2820/engine/search/Database.java
+++ src/edu/uiowa/cs2820/engine/search/Database.java
@@ -1,40 +1,46 @@
+/* Team 7
+   Members: Khashi Reyes, Justin Shroyer, Yicheng Zhao, Tao Zeyi
+*/
+
 package org.uiowa.cs2820.engine;
 
 import java.util.Vector;
 
 public class Database 
 {	
-	private static Vector<Data> v;
-	
-	public void store(String field, String id) 
+	private Vector<Data> v;
+	public Database() 
 	{
+		v = new Vector<Data>();
+	}
+	public void store(String field, String id) {
 		Data d = new Data(field,id);
 		v.add(d);
 	}
 	
-	public String getField(int index) 
+	public String getField(int i) 
 	{
-		return v.get(index).Field();
+		return v.get(i).Field();
 	}
 	
-	public String getId(int index) 
+	public String getId(int i) 
 	{
-		return v.get(index).Id();
+		return v.get(i).Id();
 	}
 
 	class Data 
 	{
-		private String field;
-		private String id;
+		String field;
+		String id;
 		Data(String field, String id) {
 			this.field = field;
 			this.id = id;
 		}
-		String Field() 
+		public String Field() 
 		{
 			return field;
 		}
-		String Id() 
+		public String Id() 
 		{
 			return id;
 		}

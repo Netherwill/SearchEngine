e82e1357e22787044f95414f9bdf33879d5a92c9
Khashi Reyes <khashi-reyes@uiowa.edu>
2014-10-23 20:33:03 -0500

    Update databaseTest.java

--- src/edu/uiowa/cs2820/engine/test/databaseTest.java
+++ src/edu/uiowa/cs2820/engine/test/databaseTest.java
@@ -20,27 +20,27 @@ public class DatabaseTest {
 		
 		//check the first object in database
 		Field tf = new Field("Hello", "World");
-		Database.store(tf, "1-1");
+		Database.store(tf, 1);
 		assertEquals(Database.getSize(),1);
 		
 		//check the second object in database
 		Field tf1 = new Field("year", "2014");
-		Database.store(tf1, "1-2");
+		Database.store(tf1, 2);
 		assertEquals(Database.getSize(),2);
 		
 		//check adding the same object in database
 		Field tf2 = new Field("year", "2014");
-		Database.store(tf2, "2-3");
+		Database.store(tf2, 3);
 		assertEquals(Database.getSize(),2);
 		
 		//one more insertion
 		Field tf3 = new Field("ISBN", "1234567");
-		Database.store(tf3, "1-3");
+		Database.store(tf3, 4);
 		assertEquals(Database.getSize(),3);
 		
 		//check the query function return the identifier
-		assertEquals(Database.querySingleField(tf),"1-1");
-		assertEquals(Database.querySingleField(tf1),"1-22-3");
+		assertEquals(Database.querySingleField(tf),1);
+		assertEquals(Database.querySingleField(tf1),5);
 				
 	}
 

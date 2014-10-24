7b35a62dc42fa5dee9bb20472fff4e79b239648f
hjshroyer <hugh-shroyer@uiowa.edu>
2014-10-24 12:11:38 -0500

    adding FieldSearch tests

--- src/edu/uiowa/cs2820/engine/test/fieldSearchTest.java
+++ src/edu/uiowa/cs2820/engine/test/fieldSearchTest.java
@@ -1,5 +1,22 @@
-package edu.uiowa.cs2820.engine.test;
+import static org.junit.Assert.*;
+import org.uiowa.cs2820.engine.search.FieldSearch;
+import edu.uiowa.cs2820.engine.search.Field;
+import edu.uiowa.cs2820.engine.search.Database;
+import edu.uiowa.cs2820.engine.search.Indexer;
 
-public class fieldSearchTest {
+import org.junit.Test;
 
+public class FieldSearchTest {
+
+	@Test
+	public void FieldSearchTestA() {
+		Field f = new Field("President", "Washington");
+		Database.store(f, 1);
+		assertEquals(FieldSearch.findEquals("Washington"),1);
+		
+	}
+	@Test
+	public void FieldSearchTestB(){
+		assertEquals(FieldSearch.findEquals("Jefferson"), "Not Found");
+	}
 }

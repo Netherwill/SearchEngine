51000849f042b79da8cef9d12d0b83abafc702f1
Khashi Reyes <khashi-reyes@uiowa.edu>
2014-10-23 20:24:53 -0500

    Update databaseTest.java

--- src/edu/uiowa/cs2820/engine/test/databaseTest.java
+++ src/edu/uiowa/cs2820/engine/test/databaseTest.java
@@ -15,33 +15,32 @@ public class DatabaseTest {
 	@Test
 	public void databaseInsertationTest() throws Exception{
 		//new test database
-		Database tdb= new Database();
 		//check the empty database size 0
-		assertEquals(tdb.getSize(),0);
+		assertEquals(Database.getSize(),0);
 		
 		//check the first object in database
 		Field tf = new Field("Hello", "World");
-		tdb.store(tf, "1-1");
-		assertEquals(tdb.getSize(),1);
+		Database.store(tf, "1-1");
+		assertEquals(Database.getSize(),1);
 		
 		//check the second object in database
 		Field tf1 = new Field("year", "2014");
-		tdb.store(tf1, "1-2");
-		assertEquals(tdb.getSize(),2);
+		Database.store(tf1, "1-2");
+		assertEquals(Database.getSize(),2);
 		
 		//check adding the same object in database
 		Field tf2 = new Field("year", "2014");
-		tdb.store(tf2, "2-3");
-		assertEquals(tdb.getSize(),2);
+		Database.store(tf2, "2-3");
+		assertEquals(Database.getSize(),2);
 		
 		//one more insertion
 		Field tf3 = new Field("ISBN", "1234567");
-		tdb.store(tf3, "1-3");
-		assertEquals(tdb.getSize(),3);
+		Database.store(tf3, "1-3");
+		assertEquals(Database.getSize(),3);
 		
 		//check the query function return the identifier
-		assertEquals(tdb.querySingleField(tf),"1-1");
-		assertEquals(tdb.querySingleField(tf1),"1-22-3");
+		assertEquals(Database.querySingleField(tf),"1-1");
+		assertEquals(Database.querySingleField(tf1),"1-22-3");
 				
 	}
 
